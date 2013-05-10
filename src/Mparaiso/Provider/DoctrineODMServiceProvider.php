df90f0be0d133dc9f30e737fc136eb450008f507
mparaiso <aikah@free.fr>
2013-05-10 12:20:40 +0200

    proxy-fix 10/05/2013

--- src/Mparaiso/Provider/DoctrineODMServiceProvider.php
+++ src/Mparaiso/Provider/DoctrineODMServiceProvider.php
@@ -12,6 +12,7 @@ use Mparaiso\Doctrine\ODM\MongoDB\Command\InfoDoctrineODMCommand;
 use Mparaiso\Doctrine\ODM\MongoDB\ManagerRegistry;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntityValidator;
 
 class DoctrineODMServiceProvider implements ServiceProviderInterface
 {
@@ -43,6 +44,11 @@ class DoctrineODMServiceProvider implements ServiceProviderInterface
                     $app['odm.config']->setMetadataDriverImpl($app['odm.chain_driver']);
                     $dm = DocumentManager::create($app['odm.connection'],
                                     $app['odm.config']);
+                    # fix the proxy serialization bug @TODO fix it
+                    $proxies = glob($app['odm.proxy_dir'] . "/__CG__*.php");
+                    foreach ($proxies as $proxy) {
+                        require($proxy);
+                    }
                     return $dm;
                 });
         $app['odm.connection.class'] = function () {
@@ -82,6 +88,9 @@ class DoctrineODMServiceProvider implements ServiceProviderInterface
         $app['odm.boot_commands'] = $app->protect(function()use($app) {
                     $app['console']->add(new InfoDoctrineODMCommand);
                 });
+        $app['doctrine_odm.mongodb.unique'] = function($app) {
+                    return new UniqueEntityValidator($app['odm.manager_registry']);
+                };
     }
 
     static function getMetadataDriver($type, $classpath) {
@@ -102,7 +111,11 @@ class DoctrineODMServiceProvider implements ServiceProviderInterface
      * {@inheritdoc}
      */
     public function boot(Application $app) {
-        // TODO: Implement boot() method.
+        $arr = $app['validator.validator_service_ids'];
+        $arr['doctrine_odm.mongodb.unique'] = 'doctrine_odm.mongodb.unique';
+        if (!isset($arr['doctrine.orm.validator.unique']))
+            $arr['doctrine.orm.validator.unique'] = 'doctrine_odm.mongodb.unique';
+        $app['validator.validator_service_ids'] = $arr;
     }
 
 }
